2373c2a84a116d584611ab2de4c39f733db0e738
vanh01 <ngvanhfw@gmail.com>
2021-11-27 20:07:41 +0700

    test api

---
+++ server/Models/SinhVien.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace DataBase_SinhVien
+{
+    public class SinhVien
+    {
+        public string MSSV { get; set; }
+        public string Ho { get; set; }
+        public string Ten { get; set; }
+        public string TinhTrang { get; set; }
+        public DateTime NgaySinh { get; set; }
+        public string GioiTinh { get; set; }
+        public string HoKhau { get; set; }
+        public string Khoa { get; set; }
+    }
+}
---
+++ server/SqlExecutes.cs
@@ -0,0 +1,84 @@
+using System.Reflection;
+using System.Collections.Generic;
+using System;
+using System.Data.SqlClient;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace DataBase_SinhVien
+{
+    public class SqlExecutes
+    {
+        private string sqlDataSource = "Data Source=.;Initial Catalog=DBASS2; Integrated Security=true";
+        static private SqlExecutes instance;
+        static public SqlExecutes Instance
+        {
+            get { if (instance == null) instance = new SqlExecutes(); return instance; }
+            private set { }
+        }
+
+        public SqlExecutes() { }
+
+        public async Task<DataTable> ExecuteQuery(string query)
+        {
+            DataTable table = new DataTable();
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                SqlDataReader myReader;
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = await myCommand.ExecuteReaderAsync();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return table;
+        }
+
+        public async Task<int> ExecuteNonQuery(string query)
+        {
+            int numberOfRows = 0;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    numberOfRows = await myCommand.ExecuteNonQueryAsync();
+                    myCon.Close();
+                }
+            }
+            return numberOfRows;
+        }
+    }
+
+
+    public static class TableExtensions
+    {
+        public static List<T> ConvertToList<T>(this DataTable dataTable)
+        {
+            List<T> data = new List<T>();
+            foreach (DataRow row in dataTable.Rows)
+            {
+                T item = GetItem<T>(row);
+                data.Add(item);
+            }
+            return data;
+        }
+        private static T GetItem<T>(DataRow dr)
+        {
+            Type temp = typeof(T);
+            T obj = Activator.CreateInstance<T>();
+
+            foreach (PropertyInfo pro in temp.GetProperties())
+            {
+                pro.SetValue(obj, dr[pro.Name], null);
+            }
+            return obj;
+        }
+    }
+
+
+}
