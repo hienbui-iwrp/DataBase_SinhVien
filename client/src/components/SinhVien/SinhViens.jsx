c35fbef82e527b5fb6c7cdaafc36c8b01bce5a24
vanh01 <ngvanhfw@gmail.com>
2021-11-29 08:05:33 +0700

    a

---
+++ client/src/components/SinhVien/SinhViens.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import ThoiKhoaBieu from "./ThoiKhoaBieu";
+
+const SinhViens = ({ Students, search, sex, faculty }) => {
+    return (
+        <>
+            {Students.map((student, index) => {
+                if (
+                    student.mssv.toString().includes(search) ||
+                    student.ten.toLowerCase().includes(search)
+                )
+                    if (sex === "Giới tính" || sex === student.gioiTinh)
+                        if (faculty === "Khoa" || faculty === student.khoa)
+                            return (
+                                <tr key={index}>
+                                    <td>{student.mssv}</td>
+                                    <td>{student.ho}</td>
+                                    <td>{student.ten}</td>
+                                    <td>{student.tinhTrang}</td>
+                                    <td>{student.ngaySinh}</td>
+                                    <td>{student.gioiTinh}</td>
+                                    <td>{student.hoKhau}</td>
+                                    <td>{student.khoa}</td>
+                                    <td>
+                                        <ThoiKhoaBieu mssv={student.mssv} />
+                                    </td>
+                                </tr>
+                            );
+            })}
+        </>
+    );
+};
+
+export default SinhViens;
