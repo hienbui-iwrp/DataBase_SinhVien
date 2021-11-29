de4342cd836351075caf795198f46da8a3662ad0
vanh01 <ngvanhfw@gmail.com>
2021-11-29 11:20:40 +0700

    update giaovien

--- client/src/components/GiaoVien/GiaoVien.jsx
+++ client/src/components/GiaoVien/GiaoVien.jsx
@@ -11,9 +11,26 @@ const GiaoVien = () => {
 
     const InsertGiaoVien = () => {};
 
-    const fetchData = () => {};
+    const fetchData = () => {
+        var requestOptions = {
+            method: "GET",
+            redirect: "follow",
+        };
 
-    useEffect(() => {}, []);
+        fetch("https://localhost:5001/api/sinhvien", requestOptions)
+            .then((response) => response.json())
+            .then((result) => setTeachers(result))
+            .catch((error) => console.log("error", error));
+
+        fetch("https://localhost:5001/api/sinhvien/khoa", requestOptions)
+            .then((response) => response.json())
+            .then((result) => setFaculties(result))
+            .catch((error) => console.log("error", error));
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, []);
 
     return (
         <>
